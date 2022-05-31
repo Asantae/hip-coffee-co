8da55f2da2dce86dca21fcf3b71ebf2335611ff4
Asantae Shelton <asantaems@gmail.com>
2022-05-31 17:31:06 -0400

    created additional forms and finished form validation for each. also added/updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# anodic-website
-Full stack web-application/shop where the user can buy different listed items via different payment methods
+# Restaurant POS
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+
+## Overview
+
+- this is a point of sale system that will allow cashiers to take coffee orders from customers with their names, allow baristas to log into the app and see orders that have been made, and mark them as complete. Orders that have been completed will note which barista completed the order. 
+### Screenshot
+
+![](./images/desktop-version.png)
+![](./images/mobile-version.png)
+
+### Links
+
+- GIthub URL: [https://github.com/Asantae/]
+- Live Site URL: []
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS3
+- Javascript
+- Node.js
+
+### What I learned
+
+```
+database creation and utility
+```
+### Continued development
+
+```
+may add more features later such as customer self-serve options or guest log in (with limited actions)
+```
+### Useful resources
+
+- [Example resource 1](https://stackoverflow.com/) - A lot of the solutions that I couldn't figure out on my own were from this site. It also provided inspiration a lot of times for different js solutions/error handling
+
+## Author
+
+- linkedIn - [@AsantaeShelton](https://www.linkedin.com/in/asantae-shelton-572467229/)
+- Twitter - [@AsantaeMS](https://www.twitter.com/AsantaeMS)
+- Github - [@Asantae](https://www.github.com/Asantae)
--- js/login.js
+++ js/login.js
@@ -1,33 +1,82 @@
 //get data
-
 let loginButton = document.querySelectorAll('#login-forms-button');
+let select = document.querySelector('select');
 const adminForm = document.querySelector('#admin-form');
+const adminLogin = document.querySelector('#admin-login');
 const cashierForm = document.querySelector('#cashier-form');
+const cashierLogin = document.querySelector('#cashier-login');
 const baristaForm = document.querySelector('#barista-form');
-const usernameInput = document.querySelector("#form-name");
-const passwordInput = document.querySelector("#form-password");
+const baristaLogin = document.querySelector('#barista-login');
+const usernameInput = document.querySelectorAll(".form-name");
+const passwordInput = document.querySelectorAll(".form-password");
 const errorNodes = document.querySelectorAll(".error");
 const loginOptions = document.querySelectorAll('option');
 const optionsArray = Array.from(loginOptions);
-for(var i = 0; i < optionsArray.length; i++){
-    optionsArray[i].addEventListener("change", console.log(`clicked ${optionsArray[i].innerText}`))
-}
+cashierLogin.classList.add('show');
+
+//select form
+select.addEventListener('change', () => {
+    adminLogin.classList.remove('show');
+    baristaLogin.classList.remove('show');
+    cashierLogin.classList.remove('show');
+    switch (select.value) {
+        case ('admin-login'):
+           adminLogin.classList.add('show');
+           break;
+        case 'cashier-login':
+            cashierLogin.classList.add('show');
+            break;
+        case 'barista-login':
+            baristaLogin.classList.add('show');
+    }    
+});
 
 //validate data
-
-function validateForm(){
+function validateForm(form){
     let errorFlag = false;
-    if(usernameInput.value.length < 1){
-        errorNodes[0].innerText = "Username cannot be blank";
-        usernameInput.classList.add("error-border");
-        errorFlag = true;
+    if(form == 0){
+        if(usernameInput[form].value.length < 1){
+            errorNodes[0].innerText = "Username cannot be blank";
+            usernameInput[0].classList.add("error-border");
+            errorFlag = true;
+        }
+        if(passwordInput[form].value.length < 1){
+            errorNodes[1].innerText = "Password cannot be blank";
+            passwordInput[0].classList.add("error-border");
+            errorFlag = true;
+        }
     }
-    if(passwordInput.value.length < 1){
-        errorNodes[1].innerText = "Password cannot be blank";
-        passwordInput.classList.add("error-border");
-        errorFlag = true;
+    else if(form == 1){
+        if(usernameInput[form].value.length < 1){
+            errorNodes[2].innerText = "Username cannot be blank";
+            usernameInput[1].classList.add("error-border");
+            errorFlag = true;
+        }
+        if(passwordInput[form].value.length < 1){
+            errorNodes[3].innerText = "Password cannot be blank";
+            passwordInput[1].classList.add("error-border");
+            errorFlag = true;
+        }
     }
-    if(!errorFlag){
+    else if(form == 2){
+        if(usernameInput[form].value.length < 1){
+            errorNodes[4].innerText = "Username cannot be blank";
+            usernameInput[2].classList.add("error-border");
+            errorFlag = true;
+        }
+        if(passwordInput[form].value.length < 1){
+            errorNodes[5].innerText = "Password cannot be blank";
+            passwordInput[2].classList.add("error-border");
+            errorFlag = true;
+        }
+    }
+    if(!errorFlag && select.value == 'admin-login'){
         adminForm.submit();
+    }
+    if(!errorFlag && select.value == 'barista-login'){
+        baristaForm.submit();
+    }
+    if(!errorFlag && select.value == 'cashier-login'){
+        cashierForm.submit();
     }
 }
